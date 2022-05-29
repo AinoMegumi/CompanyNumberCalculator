7dffd7317eb187c88000b1fa72702ef7586efd91
AinoMegumi <aimegu2014@gmail.com>
2022-05-29 16:26:37 +0900

    テストケース名の修正

--- Cpp/test.cpp
+++ Cpp/test.cpp
@@ -40,43 +40,43 @@ inline void Test_CalcCompanyNumber(const std::string& src, const std::string& Co
 }
 
 // 半角数字12文字かどうかの判定テスト
-IUTEST_PMZ(ImplMethodTest_AllValueIsHalfNumberText, Equal0, Test_AllValueIsHalfNumberText, TestCaseStrings::CorrectCase1, true);
-IUTEST_PMZ(ImplMethodTest_AllValueIsHalfNumberText, Equal1, Test_AllValueIsHalfNumberText, TestCaseStrings::CorrectCase2, false);
-IUTEST_PMZ(ImplMethodTest_AllValueIsHalfNumberText, Equal2, Test_AllValueIsHalfNumberText, TestCaseStrings::CorrectCase3, false);
-IUTEST_PMZ(ImplMethodTest_AllValueIsHalfNumberText, Equal3, Test_AllValueIsHalfNumberText, TestCaseStrings::InvalidCase1, false);
-IUTEST_PMZ(ImplMethodTest_AllValueIsHalfNumberText, Equal4, Test_AllValueIsHalfNumberText, TestCaseStrings::InvalidCase2, false);
+IUTEST_PMZ(ImplMethodTest, AllValueIsHalfNumberText, Test_AllValueIsHalfNumberText, TestCaseStrings::CorrectCase1, true);
+IUTEST_PMZ(ImplMethodTest, AllValueIsHalfNumberText, Test_AllValueIsHalfNumberText, TestCaseStrings::CorrectCase2, false);
+IUTEST_PMZ(ImplMethodTest, AllValueIsHalfNumberText, Test_AllValueIsHalfNumberText, TestCaseStrings::CorrectCase3, false);
+IUTEST_PMZ(ImplMethodTest, AllValueIsHalfNumberText, Test_AllValueIsHalfNumberText, TestCaseStrings::InvalidCase1, false);
+IUTEST_PMZ(ImplMethodTest, AllValueIsHalfNumberText, Test_AllValueIsHalfNumberText, TestCaseStrings::InvalidCase2, false);
 
 // 引数検査のテスト
-IUTEST_PMZ(ImplMethodTest_CheckArg, Equal0, Test_CheckArg, TestCaseStrings::CorrectCase1, true);
-IUTEST_PMZ(ImplMethodTest_CheckArg, Equal1, Test_CheckArg, TestCaseStrings::CorrectCase2, true);
-IUTEST_PMZ(ImplMethodTest_CheckArg, Equal2, Test_CheckArg, TestCaseStrings::CorrectCase3, true);
-IUTEST_PMZ(ImplMethodTest_CheckArg, Equal3, Test_CheckArg, TestCaseStrings::InvalidCase1, false);
-IUTEST_PMZ(ImplMethodTest_CheckArg, Equal4, Test_CheckArg, TestCaseStrings::InvalidCase2, false);
+IUTEST_PMZ(ImplMethodTest, CheckArg, Test_CheckArg, TestCaseStrings::CorrectCase1, true);
+IUTEST_PMZ(ImplMethodTest, CheckArg, Test_CheckArg, TestCaseStrings::CorrectCase2, true);
+IUTEST_PMZ(ImplMethodTest, CheckArg, Test_CheckArg, TestCaseStrings::CorrectCase3, true);
+IUTEST_PMZ(ImplMethodTest, CheckArg, Test_CheckArg, TestCaseStrings::InvalidCase1, false);
+IUTEST_PMZ(ImplMethodTest, CheckArg, Test_CheckArg, TestCaseStrings::InvalidCase2, false);
 
 // 半角変換のテスト
-IUTEST_PMZ(ImplMethodTest_HalfSizeStr, Equal0, Test_ConvertNumTextToHalfSizeString, TestCaseStrings::CorrectCase1, TestCaseStrings::CorrectCase1);
-IUTEST_PMZ(ImplMethodTest_HalfSizeStr, Equal1, Test_ConvertNumTextToHalfSizeString, TestCaseStrings::CorrectCase2, TestCaseStrings::CorrectCase1);
-IUTEST_PMZ(ImplMethodTest_HalfSizeStr, Equal2, Test_ConvertNumTextToHalfSizeString, TestCaseStrings::CorrectCase3, TestCaseStrings::CorrectCase1);
+IUTEST_PMZ(ImplMethodTest, ConvertNumTextToHalfSizeString, Test_ConvertNumTextToHalfSizeString, TestCaseStrings::CorrectCase1, TestCaseStrings::CorrectCase1);
+IUTEST_PMZ(ImplMethodTest, ConvertNumTextToHalfSizeString, Test_ConvertNumTextToHalfSizeString, TestCaseStrings::CorrectCase2, TestCaseStrings::CorrectCase1);
+IUTEST_PMZ(ImplMethodTest, ConvertNumTextToHalfSizeString, Test_ConvertNumTextToHalfSizeString, TestCaseStrings::CorrectCase3, TestCaseStrings::CorrectCase1);
 
 // チェックディジットの計算のテスト
-IUTEST_PMZ(ImplMethodTest_CheckArg, Equal0, Test_CalcCheckDigit, TestCaseStrings::CorrectCase1, 8);
-IUTEST_PMZ(ImplMethodTest_CheckArg, Equal1, Test_CalcCheckDigit, TestCaseStrings::CorrectCase2, 0);
-IUTEST_PMZ(ImplMethodTest_CheckArg, Equal2, Test_CalcCheckDigit, TestCaseStrings::CorrectCase3, 0);
-IUTEST_PMZ(ImplMethodTest_CheckArg, Equal3, Test_CalcCheckDigit, TestCaseStrings::InvalidCase1, 0);
-IUTEST_PMZ(ImplMethodTest_CheckArg, Equal4, Test_CalcCheckDigit, TestCaseStrings::InvalidCase2, 0);
+IUTEST_PMZ(ImplMethodTest, CalcCheckDigit, Test_CalcCheckDigit, TestCaseStrings::CorrectCase1, 8);
+IUTEST_PMZ(ImplMethodTest, CalcCheckDigit, Test_CalcCheckDigit, TestCaseStrings::CorrectCase2, 0);
+IUTEST_PMZ(ImplMethodTest, CalcCheckDigit, Test_CalcCheckDigit, TestCaseStrings::CorrectCase3, 0);
+IUTEST_PMZ(ImplMethodTest, CalcCheckDigit, Test_CalcCheckDigit, TestCaseStrings::InvalidCase1, 0);
+IUTEST_PMZ(ImplMethodTest, CalcCheckDigit, Test_CalcCheckDigit, TestCaseStrings::InvalidCase2, 0);
 
 // チェックディジットと会社法人等番号から法人番号の生成のテスト
-IUTEST_PMZ(ImplMethodTest_GenerateCompanyNumber, Equal0, Test_GenerateCompanyNumber, 8, TestCaseStrings::CorrectCase1, GeneratedCompanyNumber);
-IUTEST_PMZ(ImplMethodTest_GenerateCompanyNumber, Equal1, Test_GenerateCompanyNumber, 8, TestCaseStrings::CorrectCase2, "");
-IUTEST_PMZ(ImplMethodTest_GenerateCompanyNumber, Equal2, Test_GenerateCompanyNumber, 8, TestCaseStrings::CorrectCase3, "");
-IUTEST_PMZ(ImplMethodTest_GenerateCompanyNumber, Equal3, Test_GenerateCompanyNumber, 8, TestCaseStrings::InvalidCase1, "");
-IUTEST_PMZ(ImplMethodTest_GenerateCompanyNumber, Equal4, Test_GenerateCompanyNumber, 8, TestCaseStrings::InvalidCase2, "");
-IUTEST_PMZ(ImplMethodTest_GenerateCompanyNumber, Equal0, Test_GenerateCompanyNumber, 0, TestCaseStrings::CorrectCase1, "");
-IUTEST_PMZ(ImplMethodTest_GenerateCompanyNumber, Equal0, Test_GenerateCompanyNumber, 10, TestCaseStrings::CorrectCase1, "");
+IUTEST_PMZ(ImplMethodTest, GenerateCompanyNumber, Test_GenerateCompanyNumber, 8, TestCaseStrings::CorrectCase1, GeneratedCompanyNumber);
+IUTEST_PMZ(ImplMethodTest, GenerateCompanyNumber, Test_GenerateCompanyNumber, 8, TestCaseStrings::CorrectCase2, "");
+IUTEST_PMZ(ImplMethodTest, GenerateCompanyNumber, Test_GenerateCompanyNumber, 8, TestCaseStrings::CorrectCase3, "");
+IUTEST_PMZ(ImplMethodTest, GenerateCompanyNumber, Test_GenerateCompanyNumber, 8, TestCaseStrings::InvalidCase1, "");
+IUTEST_PMZ(ImplMethodTest, GenerateCompanyNumber, Test_GenerateCompanyNumber, 8, TestCaseStrings::InvalidCase2, "");
+IUTEST_PMZ(ImplMethodTest, GenerateCompanyNumber, Test_GenerateCompanyNumber, 0, TestCaseStrings::CorrectCase1, "");
+IUTEST_PMZ(ImplMethodTest, GenerateCompanyNumber, Test_GenerateCompanyNumber, 10, TestCaseStrings::CorrectCase1, "");
 
 // 総合テスト
-IUTEST_PMZ(MainMethodTest, Equal0, Test_CalcCompanyNumber, TestCaseStrings::CorrectCase1, GeneratedCompanyNumber);
-IUTEST_PMZ(MainMethodTest, Equal1, Test_CalcCompanyNumber, TestCaseStrings::CorrectCase2, GeneratedCompanyNumber);
-IUTEST_PMZ(MainMethodTest, Equal2, Test_CalcCompanyNumber, TestCaseStrings::CorrectCase3, GeneratedCompanyNumber);
-IUTEST_PMZ(MainMethodTest, Equal3, Test_CalcCompanyNumber, TestCaseStrings::InvalidCase1, "");
-IUTEST_PMZ(MainMethodTest, Equal4, Test_CalcCompanyNumber, TestCaseStrings::InvalidCase2, "");
+IUTEST_PMZ(MainMethodTest, CalcCompanyNumber, Test_CalcCompanyNumber, TestCaseStrings::CorrectCase1, GeneratedCompanyNumber);
+IUTEST_PMZ(MainMethodTest, CalcCompanyNumber, Test_CalcCompanyNumber, TestCaseStrings::CorrectCase2, GeneratedCompanyNumber);
+IUTEST_PMZ(MainMethodTest, CalcCompanyNumber, Test_CalcCompanyNumber, TestCaseStrings::CorrectCase3, GeneratedCompanyNumber);
+IUTEST_PMZ(MainMethodTest, CalcCompanyNumber, Test_CalcCompanyNumber, TestCaseStrings::InvalidCase1, "");
+IUTEST_PMZ(MainMethodTest, CalcCompanyNumber, Test_CalcCompanyNumber, TestCaseStrings::InvalidCase2, "");
