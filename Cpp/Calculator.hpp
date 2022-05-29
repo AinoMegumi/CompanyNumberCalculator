bf082eff0767661bdca8b1c203eee36e6af3d352
AinoMegumi <aimegu2014@gmail.com>
2022-05-29 16:28:07 +0900

    各桁の和の計算部を変更

--- Cpp/Calculator.hpp
+++ Cpp/Calculator.hpp
@@ -30,13 +30,11 @@ namespace CalculationImpl {
 
     inline unsigned long CalcCheckDigit(const std::string& val) {
         if (!AllValueIsHalfNumberText(val)) return {};
-
-        unsigned long TotalBuf[2] = { 0, 0 };
-        for (int i = 0; i < 6; i++) {
-            TotalBuf[0] += static_cast<unsigned long>(val[i * 2] - '0');
-            TotalBuf[1] += static_cast<unsigned long>(val[i * 2 + 1] - '0');
+        unsigned int Total = 0;
+        for (auto i = val.begin(); i < val.end(); i += 2) {
+            Total += static_cast<unsigned long>(*i - '0') * 2 + static_cast<unsigned long>(*(i + 1) - '0');
         }
-        return 9 - ((TotalBuf[0] * 2 + TotalBuf[1]) % 9);
+        return 9 - (Total % 9);
     }
 
     inline std::string GenerateCompanyNumber(const unsigned long& CheckDigit, const std::string& val) {
